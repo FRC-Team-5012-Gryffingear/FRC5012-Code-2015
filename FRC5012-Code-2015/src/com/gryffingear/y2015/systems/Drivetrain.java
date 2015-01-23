72795608e385b21d47dec275199631e9539e4ea0
Jeremy Paul Germita <jeremy.germita@gmail.com>
2015-01-23 13:22:06 -0800

    Tiny todo in drivetrain.java.

--- FRC5012-Code-2015/src/com/gryffingear/y2015/systems/Drivetrain.java
+++ FRC5012-Code-2015/src/com/gryffingear/y2015/systems/Drivetrain.java
@@ -13,7 +13,6 @@ public class Drivetrain {
 		leftb = new Victor(lb);
 		righta = new Victor(ra);
 		rightb = new Victor(rb);
-
 	}
 
 	public void tankDrive(double leftv, double rightv) {
@@ -22,5 +21,17 @@ public class Drivetrain {
 
 		righta.set(rightv);
 		rightb.set(rightv);
+	}
+	
+	
+	/**
+	 * method to loosely couple teleop drive logic from drive control logic. 
+	 * @param leftIn
+	 * @param rightIn
+	 */
+	public void teleopDrive(double leftIn, double rightIn) {
+		
+		// Todo: implement negative-negative inertia or similar ramping here. -JPG
+		tankDrive(leftIn, rightIn);
 	}
 }
